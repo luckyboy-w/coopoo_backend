a87f0f7ffce2c321885cdda55c33fde1032dde27
CWX <m15291337738@163.com>
2021-01-27 15:57:30 +0800

    加删除确认弹框

--- src/views/goods/desc/index.vue
+++ src/views/goods/desc/index.vue
@@ -130,9 +130,19 @@ export default {
       this.showPagination = this.tableData.total == 0;
     },
     async deleteRow(rowData, data) {
-      const res = await deleteMethod(`/backend/goodSalesDesc/${rowData.id}`)
-      this.loadList();
-      this.$message.success("删除成功");
+      try {
+        await this.$confirm('此操作将永久删除, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        })
+
+        const res = await deleteMethod(`/backend/goodSalesDesc/${rowData.id}`)
+        this.loadList();
+        this.$message.success("删除成功");
+
+      } catch (e) {
+      }
     },
     async addOrEdit(oper, rowIndex, data) {
 
