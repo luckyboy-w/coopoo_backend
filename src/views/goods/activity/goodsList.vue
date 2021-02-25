eb1600b92f3447b16124a172a2568ba74b12786c
francis <francis@coopoo.com>
2021-02-25 15:56:12 +0800

    添加活动商品列表

---
+++ src/views/goods/activity/goodsList.vue
@@ -0,0 +1,188 @@
+<template>
+  <div>
+    <div style="padding:20px 10px">
+      <div class="ly-tool-panel">
+        <table>
+          <tr>
+            <td>商品名称:</td>
+            <td>
+              <el-input v-model="searchParam.goodName" width="180px"></el-input>
+            </td>
+            <td>供应商名称：</td>
+            <td>
+              <el-select v-model="searchParam.tenantId" placeholder="请选择">
+                <el-option value="" label="全部"/>
+                <el-option
+                  v-for="item in supplyList"
+                  :key="item.id"
+                  :label="item.supplierName"
+                  :value="item.id"
+                />
+              </el-select>
+            </td>
+            <td>活动有效期</td>
+            <td>
+              <el-date-picker v-model="searchParam.activityStartTime" value-format="yyyy-MM-dd" type="date" placeholder="选择开始日期"/>
+            </td>
+            <td style="text-align: center;">-</td>
+            <td>
+              <el-date-picker v-model="searchParam.activityEndTime" value-format="yyyy-MM-dd" type="date" placeholder="选择结束日期"/>
+            </td>
+            <td>
+              <el-button icon="el-icon-search" @click="search()">查询</el-button>
+              <el-button plain type="primary" @click="addOrEdit('add')" icon="el-icon-document-add">新增</el-button>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div class="ly-table-panel" v-loading="isLoading">
+        <el-table ref="mainTable" :data="tableData.list" row-key="id"
+                  border :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
+          <el-table-column type="expand">
+            <template slot-scope="props">
+              <el-table :data="props.row.goodSkuValList" style="width: 100%; margin-bottom: 20px;" border>
+                <el-table-column prop="skuText" label="sku属性"/>
+                <el-table-column prop="marketingStock" label="库存"/>
+                <el-table-column prop="salePrice" label="零售价"/>
+                <el-table-column prop="marketingPrice" label="活动价"/>
+              </el-table>
+            </template>
+          </el-table-column>
+          <el-table-column prop="goodName" label="商品名称"/>
+          <el-table-column prop="activityName" label="活动名称"/>
+          <el-table-column prop="supplierName" label="供应商名称" width="400px"/>
+          <el-table-column prop="supplierRebateRatio" label="供应商比例" width="400px">
+            <template slot-scope="scope">
+              {{ scope.row.supplierRebateRatio}}%
+            </template>
+          </el-table-column>
+          <el-table-column prop="providerRebateRatioE" label="E类服务商比例" width="400px">
+            <template slot-scope="scope">
+              {{ scope.row.providerRebateRatioE}}%
+            </template>
+          </el-table-column>
+          <el-table-column label="活动有效期" width="400px">
+            <template slot-scope="scope">
+              {{ scope.row.activityStartTime | _formateDate}} 至 {{scope.row.activityEndTime | _formateDate }}
+            </template>
+          </el-table-column>
+          <el-table-column label="操作">
+            <template slot-scope="scope">
+              <el-button type="text" size="small" @click.native.prevent="addOrEdit('edit', scope.row)">修改</el-button>
+              <el-button type="text" size="small" @click.native.prevent="addGood(scope.row)">新增商品</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <div class="ly-data-pagination">
+          <el-pagination v-show="!showPagination" :total="tableData.total" background layout="prev, pager, next"
+                         @current-change="currentPage" @prev-click="currentPage" @next-click="currentPage" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getMethod, postMethod } from '@/api/request'
+import { formatDate } from "@/api/tools.js"
+
+export default {
+  filters: {
+    _formateDate(time) {
+      if (time == undefined) {
+        return '';
+      }
+      let date = new Date(time);
+      return formatDate(date, 'yyyy-MM-dd')
+    }
+  },
+  name:'',
+  props: {
+    activityData: {
+      type: Object,
+      required: false
+    }
+  },
+  data () {
+    return {
+      isLoading: false,
+      showPagination: false,
+      tableData: {
+        list: []
+      },
+      supplyList: [],
+      searchParam:{
+        goodActivityId: null,
+        tenantId: null
+      }
+    };
+  },
+  components: {},
+  computed: {},
+  beforeMount() {},
+
+  mounted() {
+    console.info(this.activityData)
+    this.searchParam.goodActivityId = this.activityData.id;
+    this.initSupplyList();
+    this.loadList();
+  },
+
+  methods: {
+    initSupplyList() {
+      getMethod('/backend/supplier/findList', {}).then(res => {
+        console.info(res.data)
+        this.supplyList = res.data
+      })
+    },
+
+    loadList() {
+      const scope = this;
+      if (!scope.isLoading) {
+        scope.isLoading = true
+      }
+      getMethod("/backend/goodActivity/findMarketingGood", this.searchParam).then(res => {
+        scope.tableData = res.data;
+        scope.isLoading = false
+        scope.showPagination = scope.tableData.total == 0;
+      });
+    },
+
+    search() {
+      this.loadList();
+    },
+
+    currentPage(pageNum) {
+      this.searchParam.pageNum = pageNum;
+      this.loadList();
+    }
+
+  },
+
+  watch: {}
+
+}
+
+</script>
+<style lang="scss" scoped>
+  .ly-container {
+    padding: 10px 20px;
+    font-size: 14px;
+
+    .ly-tool-panel {
+      div {
+        display: inline;
+      }
+
+      line-height: "60px";
+      height: "60px";
+      width: 100%;
+      padding: 10px 10px;
+
+      .ly-tool-btn {
+        padding-left: 20px;
+        display: inline;
+      }
+    }
+  }
+</style>
