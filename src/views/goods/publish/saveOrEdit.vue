404f72554a9a18be6f2f3449cddcbd03432b1295
king <king_w318@163.com>
2021-08-09 11:01:03 +0800

    取消自提/邮寄

--- src/views/goods/publish/saveOrEdit.vue
+++ src/views/goods/publish/saveOrEdit.vue
@@ -15,7 +15,7 @@
           <el-form-item label="发货方式">
             <el-radio v-model="dataForm.deliveryMethod" label="2">邮寄到家</el-radio>
             <el-radio v-model="dataForm.deliveryMethod" label="3">到店自提</el-radio>
-            <el-radio v-model="dataForm.deliveryMethod" label="1">邮寄/自提</el-radio>
+            <!-- <el-radio v-model="dataForm.deliveryMethod" label="1">邮寄/自提</el-radio> -->
           </el-form-item>
           <el-form-item label="属性选择">
             <template v-for="item in dbAttrList">
