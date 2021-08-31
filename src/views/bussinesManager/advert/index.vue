3c9e1beed09a9b03e69470d229d420d24e281625
king <king_w318@163.com>
2021-08-31 09:59:19 +0800

    广告管理添加跳转邀请好友

--- src/views/bussinesManager/advert/index.vue
+++ src/views/bussinesManager/advert/index.vue
@@ -39,6 +39,7 @@
                 <span v-if="scope.row.dataType==7">平台直播</span>
                 <span v-if="scope.row.dataType==8">优惠卷</span>
                 <span v-if="scope.row.dataType==9">新人礼</span>
+                <span v-if="scope.row.dataType==10">邀请好友</span>
               </template>
             </el-table-column>
             <el-table-column prop="image" label="BANNER图" width="350px">
